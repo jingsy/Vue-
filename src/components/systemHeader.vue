f98e6bee302bdb030012f7a8ecda45da11693bbc
Shuyan Jing <sjjing@student.unimelb.edu.au>
2020-04-13 20:43:16 +1000

    学生管理系统

---
+++ src/components/systemHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- 头部 -->
+    <div class="header">
+      <div class="logo">
+        <img src="https://www.adweek.com/wp-content/uploads/2019/01/mastercard-new-logo-content-2019.jpg" style="border-radius:20%" alt />&nbsp;
+        <span>学生后台管理系统</span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
